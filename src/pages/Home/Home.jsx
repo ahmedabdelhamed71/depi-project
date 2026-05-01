f3671fa67159e626f045fa74e1245bc3055a0420
Aya Saber <aya.sabercollege@gmail.com>
2026-05-01 13:07:29 +0300

    finish home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import { Button } from "@material-tailwind/react";
+import { Camera , Code, PenTool , Globe , TrendingUp} from 'lucide-react';
 
 const Home = () => {
+
+  const Skills = [
+    {
+      title: 'Web Development',
+      members: '1,245 members',
+      icon: <Code size={40} color="#204ccf" />
+    },
+    {
+      title: 'Design',
+      members: '982 members',
+      icon: <PenTool size={40} color="#204ccf" />
+    },
+    {
+      title: 'Language Learning',
+      members: '1,152 members',
+      icon: <Globe size={40} color="#204ccf" />
+    },
+    {
+      title: 'Photography',
+      members: '845 members',
+      icon: <Camera size={40} color="#204ccf" />
+    },
+    {
+      title: 'Marketing',
+      members: '1,035 members',
+      icon: <TrendingUp size={40} color="#204ccf" />
+    }
+  ];
+
   return (
-    <div className='container py-20 mx-auto'>
+    <div className='container pt-20 pb-10 mx-auto'>
       <section className='flex flex-row justify-evenly items-center mx-auto'>
         <div className='flex flex-col'>
-          <div className='flex flex-col text-3xl font-bold'>
+          <div className='flex flex-col text-4xl font-bold'>
             <h1>Learn Skills.</h1>
             <h1>Teach Skills.</h1>
             <h1>Swap Growth.</h1>
@@ -20,9 +50,34 @@ const Home = () => {
           </div>
         </div>
         {/* <div className='flex justify-content-end'> */}
-          <img src='https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlJTIwbGVhcm5pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60' alt='home' className='rounded-lg object-cover max-w-[100%] w-[35%]' />
+        <img src='https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlJTIwbGVhcm5pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60' alt='home' className='rounded-lg object-cover max-w-[100%] w-[35%]' />
         {/* </div> */}
         <div>
+
+        </div>
+      </section>
+      <section className='mt-12'>
+        <p className='text-2xl font-bold'>
+          Popular skills
+        </p>
+        <div className='flex flex-row mt-4 gap-4'>
+          {Skills.map((value, index) =>(
+          <div key={index} className='flex flex-row gap-4 rounded-lg shadow-lg px-4 pt-2 pb-4 pe-7'>
+            <div className='bg-blue-50 rounded-lg p-1 mt-2 d-flex items-center px-2'>
+              {value.icon}
+            </div>
+            <div className='flex flex-col'>
+              <p className='text-lg font-bold'>
+                {value.title}
+              </p>
+              <p>
+                {value.members}
+              </p>
+
+            </div>
+            
+          </div>
+        ))}
 
         </div>
       </section>
